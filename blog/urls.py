d2b41dbbec2fd2f69d6e893be61062947fdb4002
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2018-09-30 20:42:36 +0200

    Blog templates, styling and dummy posts rendering

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog_home')
+    path('', views.home, name='blog_home'),
+    path('blog/', views.about, name='blog_about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Denis Kotlyarchuk',
+        'title': 'My First Post',
+        'content': "Yeah, I did it! My first post",
+        'date_posted': "August 18, 2018"
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Second Post',
+        'content': "Second post content bruh",
+        'date_posted': "August 19, 2018"
+    },
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    return render(request, 'blog/home.html', {'posts': posts})
+
+
+def about(request):
+    return render(request, 'blog/about.html')
