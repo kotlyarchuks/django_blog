d525c533465553055928650d241d4de6d59c6871
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2018-10-01 17:29:42 +0200

    Post model, rendering posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Denis Kotlyarchuk',
-        'title': 'My First Post',
-        'content': "Yeah, I did it! My first post",
-        'date_posted': "August 18, 2018"
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Second Post',
-        'content': "Second post content bruh",
-        'date_posted': "August 19, 2018"
-    },
-]
+from .models import Post
 
 
 def home(request):
-    return render(request, 'blog/home.html', {'posts': posts})
+    return render(request, 'blog/home.html', {'posts': Post.objects.all()})
 
 
 def about(request):
